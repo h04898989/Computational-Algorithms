29d72fc27a3044728c5cfa0d4ecb78755e6ad06c
h04898989 <30618021+h04898989@users.noreply.github.com>
2020-11-25 01:22:40 +0800

    最小sample rate

--- RS/RSpattern.py
+++ RS/RSpattern.py
@@ -4,8 +4,9 @@ Created on Thu Aug 27 01:02:08 2020
 
 @author: dengcheng
 """
-from RSfunction import fit, sampling,findheader
+from RSfunction import*# fit, sampling,findheader
 import matplotlib.pyplot as plt
+'''
 import cv2
 capture = cv2.VideoCapture("D:\Data\RSvideo\RSvideo.mp4")
 
@@ -32,6 +33,11 @@ del temp, ret, i
 
 nf = 0 #幀數
 colmatrix[nf] = list(colmatrix[nf])#把第x幀的colmatrix變成list
+'''
+height = 480 #height, nf, colmatrix都可刪
+nf = 0
+colmatrix = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]]
+
 
 #進行fitting與篩選
 colmatrix_0 = fit()
@@ -48,8 +54,9 @@ plt.figure() # <Figure size 432x288 with 0 Axes>
 #plt.plot(range(height),colmatrix_1.getThreshold(),lw=0.3) #第二次fitting
 #plt.plot(range(height),colmatrix_1.getOutput(),lw=0.3) #第二次篩選後
 #plt.plot(range(height),colmatrix_2.getThreshold(),lw=0.3) #第三次fitting
-plt.plot(range(height),colmatrix_2.getOutput(),lw=0.3) #第三次篩選後
-#print(colmatrix_2.getOutput())
+#plt.plot(range(height),colmatrix_2.getOutput(),lw=0.3) #第三次篩選後
+print('93->120: ' + str(colmatrix_2.getOutput()[93:120]))
+print('320->347: ' + str(colmatrix_2.getOutput()[320:347]))
 
 
 import scipy.io as sio
@@ -57,6 +64,7 @@ matp = u"D:/Data/RSvideo/newrolling.mat"
 matfile=sio.loadmat(matp)
 T = matfile["T"][0] #len 78
 T1 = matfile["T1"][0] #len 35
+
 '''
 for i in range(len(T)):
     samwithT = samlist = sampling(orilist, len(T), i)
@@ -70,27 +78,42 @@ for i in range(len(T1)):
 #尋找各種sample rate下的header
 orilist = colmatrix_2.getOutput() #想找header的list
 header = [1,0,1,0,1,0,1,0] #想要找到的header
-findrange = int(len(orilist)/8) #縮小查找範圍用的參數
 
+#找到samplerate最小參考值
+h = hfinder()
+findrange = h.getlim(orilist, header) #縮小查找範圍用的參數
+print('limt = ' + str(h.getlim(orilist, header)))
+
+#判斷header的index
 for sl in [sl+1 for sl in range(findrange)]:
     for si in [sl for sl in range(findrange)]:
         if si<sl:
-            samlist = sampling(orilist, sl, si)
-            fhresult = findheader(samlist, header, sl, si)
-            headerindex = fhresult[0]
-            array_at_some_samplerate = fhresult[1]
-            #if headerindex!=[]:
-                #print('headerindex = ' + str(headerindex) + ', slice length = ' + str(fhresult[2]) + ', start position = ' + str(fhresult[3])+'\n')
-                #print('Number of datapoints =' + str(len(array_at_some_samplerate))) #特定取樣率下的陣列大小
-                #print('slice length = ' + str(fhresult[2])) #取樣的間距
-                #print('start position = ' + str(fhresult[3])) #起始取值點
-                #print('SignalArray at your sample rate =\n' + str(array_at_some_samplerate) + '\n')
-print('Finished. findrange = ' + str(findrange))
+            h = hfinder()
+            samlist = h.sampling(orilist, sl, si)
+            fhresult = h.findheader(samlist, header, sl, si, 0.1)
+            h.sumindex(fhresult[0])
+            print('header index: ' + str(fhresult[0]) + ', ' + str(sl) + '-' + str(si) + '\n')
+hindex = sorted(h.gethindex())
+nhindex = {s:0 for s in set(hindex)}
 
-print('T = ' + str(T))
-print('len(T) = ' + str(len(T)))
+#顯示各個headerindex出現的數量
+for s in hindex:
+    nhindex[s]+=1
+'''
+#刪除只出現過一次的
+for i in nhindex.items():
+    if i[1]<2:
+        del nhindex[i[0]]
+'''
+        
+print('Finished.' + str(nhindex))
+
+
+print('\nT = ' + str(T))
+print('len(T) = ' + str(len(T))+'\n')
 print('T1 = ' + str(T1))
 print('len(T1) = ' + str(len(T1)))
+
 
 '''
 #plt.xlim(0,5)
