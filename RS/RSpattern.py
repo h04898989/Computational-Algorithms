c92377b9a6acc26a31ffa361af01138d5257a67e
h04898989 <30618021+h04898989@users.noreply.github.com>
2020-11-30 17:58:19 +0800

    小滴最後版本

--- RS/RSpattern.py
+++ RS/RSpattern.py
@@ -4,7 +4,7 @@ Created on Thu Aug 27 01:02:08 2020
 
 @author: dengcheng
 """
-from RSfunction import*# fit, sampling,findheader
+from RSfunction import*# fit, hfinder, calerror
 import matplotlib.pyplot as plt
 '''
 import cv2
@@ -34,11 +34,12 @@ del temp, ret, i
 nf = 0 #幀數
 colmatrix[nf] = list(colmatrix[nf])#把第x幀的colmatrix變成list
 '''
+#方便測試
 height = 480 #height, nf, colmatrix都可刪
 nf = 0
-colmatrix = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]]
+colmatrix = [[31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 30, 30, 30, 26, 22, 13, 9, 2, 0, 1, 2, 8, 12, 20, 26, 30, 31, 32, 33, 33, 29, 20, 17, 13, 6, 0, 0, 0, 0, 0, 0, 0, 0, 1, 8, 9, 18, 25, 23, 23, 18, 9, 5, 4, 2, 3, 9, 13, 22, 33, 34, 34, 34, 34, 34, 32, 26, 18, 15, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 8, 15, 16, 15, 10, 6, 8, 13, 22, 18, 6, 4, 11, 16, 18, 16, 8, 6, 16, 18, 22, 22, 23, 22, 22, 20, 18, 14, 10, 0, 0, 0, 0, 0, 0, 0, 0, 5, 16, 19, 24, 36, 43, 43, 43, 43, 43, 41, 41, 41, 41, 41, 40, 37, 31, 23, 15, 2, 0, 0, 0, 0, 0, 0, 0, 0, 5, 10, 17, 31, 37, 40, 40, 40, 41, 41, 41, 41, 43, 43, 41, 38, 34, 29, 24, 12, 6, 0, 0, 4, 13, 16, 27, 34, 33, 29, 19, 11, 2, 0, 0, 0, 0, 0, 0, 0, 0, 5, 12, 26, 31, 31, 30, 20, 11, 6, 2, 0, 0, 0, 0, 0, 0, 0, 0, 15, 23, 29, 36, 40, 43, 43, 43, 43, 43, 43, 43, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 45, 44, 43, 36, 30, 26, 17, 10, 4, 5, 3, 9, 13, 18, 24, 35, 43, 42, 42, 42, 42, 36, 29, 16, 13, 5, 0, 0, 0, 0, 0, 0, 0, 0, 2, 12, 25, 23, 26, 25, 20, 16, 10, 2, 0, 0, 4, 15, 23, 25, 33, 40, 39, 37, 40, 37, 30, 23, 18, 8, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 10, 19, 19, 10, 6, 6, 15, 18, 18, 10, 8, 8, 11, 16, 15, 10, 6, 9, 15, 17, 18, 19, 20, 23, 23, 18, 10, 8, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 15, 20, 24, 26, 31, 34, 36, 36, 36, 36, 34, 34, 33, 34, 32, 26, 17, 11, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 5, 12, 19, 22, 26, 30, 31, 31, 31, 31, 31, 31, 31, 31, 30, 27, 20, 18, 12, 8, 0, 0, 0, 0, 9, 15, 13, 13, 13, 13, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 6, 11, 16, 15, 12, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 12, 16, 24, 25, 25, 26, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 25, 25, 24, 24, 24, 24, 24, 24, 24, 24, 24, 21, 12, 9]]
 
-'''
+
 #進行fitting與篩選
 colmatrix_0 = fit()
 colmatrix_0.thresholding(list(range(height)),colmatrix[nf],2)
@@ -48,41 +49,32 @@ colmatrix_2 = fit()
 colmatrix_2.thresholding(list(range(height)),colmatrix_1.getOutput(),1)
 
 plt.figure() # <Figure size 432x288 with 0 Axes>
-#plt.plot(range(height),colmatrix[nf],lw=0.3) #原本的值
+plt.plot(range(height),colmatrix_0.getInput(),lw=0.3) #原本的值
 #plt.plot(range(height),colmatrix_0.getThreshold(),lw=0.3) #第一次fitting
 #plt.plot(range(height),colmatrix_0.getOutput(),lw=0.3) #第一次篩選後
 #plt.plot(range(height),colmatrix_1.getThreshold(),lw=0.3) #第二次fitting
 #plt.plot(range(height),colmatrix_1.getOutput(),lw=0.3) #第二次篩選後
 #plt.plot(range(height),colmatrix_2.getThreshold(),lw=0.3) #第三次fitting
 #plt.plot(range(height),colmatrix_2.getOutput(),lw=0.3) #第三次篩選後
-'''
-print('93->120: ' + str(colmatrix[nf][93:120]))
-print('320->347: ' + str(colmatrix[nf][320:347]))
-
-
-import scipy.io as sio
-matp = u"D:/Data/RSvideo/newrolling.mat"
-matfile=sio.loadmat(matp)
-T = matfile["T"][0] #len 78
-T1 = matfile["T1"][0] #len 35
-
-'''
-for i in range(len(T)):
-    samwithT = samlist = sampling(orilist, len(T), i)
-    fhresult = findheader(samwithT, header, len(T), i)
-for i in range(len(T1)):
-    samwithT1 = samlist = sampling(orilist, len(T1), i)
-    fhresult = findheader(samwithT1, header, len(T1), i)
-'''
+#plt.xlim(0,5)
+#plt.ylim(-1,5)
+#plt.xlabel("x-axis")
+#plt.ylabel("y-axis")
+#plt.title("The Title")
+#plt.savefig("filename.png",dpi=300,format="png")
 
 
 #尋找各種sample rate下的header
-#orilist = [0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,1,1,1,1,1]
-#000000000011100011100011100011100011111111
-#00000000010010010010010010010010010000000
-#000111222322322322322322322322211100
-orilist = colmatrix[nf]#colmatrix_2.getOutput() #想找header的list
+orilist = colmatrix_2.getOutput() #想找header的list
 header = [1,0,1,0,1,0,1,0] #想要找到的header
+
+#讀取T和T1
+import scipy.io as sio
+matp = u"D:/Data/RSvideo/newrolling.mat"
+matfile=sio.loadmat(matp)
+print('---------------' + str(type(matfile["T"][0])))
+T = matfile["T"][0] #len 78
+T1 = matfile["T1"][0] #len 35
 
 #找到samplerate最小參考值
 h = hfinder()
@@ -94,66 +86,43 @@ rpayload = 0
 for sl in [sl+1 for sl in range(findrange)]:
     for si in [sl for sl in range(findrange)]:
         if si<sl:
-            h = hfinder()
             samlist = h.sampling(orilist, sl, si)
             fhresult = h.findheader(samlist, header, sl, si)
-            h.sumindex(fhresult[0])
+            if sl==3 and si==1:
+                print('以下顯示原陣列、difference、density的比較:('+str(sl)+'-'+str(si)+')')
+                print('原陣列(91~120):      ' + str(orilist[88:121]))
+                print('sam陣列(30~40):     ' + str(samlist[29:40]))
+                print('deifference(30~40): ' + str(fhresult[1][29:40]))
+                print('density(30~40):     ' + str(fhresult[2][29:40])+'\n')
             if h.goodslsi(fhresult[0]): #只顯示抓到兩組以上header的
-                #print('header index: ' + str(fhresult[0]) + ', ' + str(sl) + '-' + str(si))
                 sindex = h.selectindex(samlist,header,sl,si,len(T))
                 if sindex!=None:
                     rpayload = list(T)
-                    #print('totaldensity = '+str(sindex[3]))
+                    print('totaldensity = '+str(sindex[3]))
                     print('headerindex: ' + str(sindex[0]) + ' with T')
                 sindex = h.selectindex(samlist,header,sl,si,len(T1))
                 if sindex!=None:
                     rpayload = list(T1)
-                    #print('totaldensity = '+str(sindex[3]))
+                    print('totaldensity = '+str(sindex[3]))
                     print('headerindex: ' + str(sindex[0]) + ' with T1')
-                #print('maxdensity: ' + str(max(fhresult[2])) + '\n')
                 header1 = sindex[0][1]
                 header2 = sindex[0][0]
                 samlist2 = h.sampling(samlist[int((header1-si)/sl+8):int((header2-si)/sl)],sindex[0][2],0)#起點的+8是扣掉header佔據的長度
-                print('payload = ' + str(samlist2))
+                print('payload =  ' + str(samlist2))
                 print('rpayload = ' + str(rpayload))
-                print('error number = ' + str(calerror(samlist2,rpayload)))
-                '''
-                print(orilist[fhresult[0][0]-6:fhresult[0][0]+24])
-                print(samlist[int((fhresult[0][0]-si)/sl-6):int((fhresult[0][0]-si)/sl+10)])
-                print(fhresult[1][int((fhresult[0][0]-si)/sl-1):int((fhresult[0][0]-si)/sl+10)])
-                print(fhresult[2][int((fhresult[0][0]-si)/sl-1):int((fhresult[0][0]-si)/sl+10)])
-                print('\n')
-                print(orilist[fhresult[0][1]-6:fhresult[0][1]+25])
-                print(samlist[int((322-si)/sl-6):int((fhresult[0][1]-si)/sl+10)])
-                print(fhresult[1][int((fhresult[0][1]-si)/sl-1):int((fhresult[0][1]-si)/sl+10)])
-                print(fhresult[2][int((fhresult[0][1]-si)/sl-1):int((fhresult[0][1]-si)/sl+10)])
-                '''
+                print('error number = ' + str(calerror(samlist2,rpayload)) + '\n')
 print('Finished.')
 
-'''
-#顯示各個headerindex出現的數量
-hindex = sorted(h.gethindex())
-nhindex = {s:0 for s in set(hindex)}
-for s in hindex:
-    nhindex[s]+=1
-        
-print(nhindex)
-'''
+#header附近
+print('93->120: ' + str(orilist[93:120]))
+print('320->347: ' + str(orilist[320:347]))
 
 print('\nT (len='+str(len(T))+') = ' + str(T))
-print('\nT1(len='+str(len(T1))+') = ' + str(T1))
+print('T1(len='+str(len(T1))+') = ' + str(T1))
 print()
 
 
-'''
-#plt.xlim(0,5)
-#plt.ylim(-1,5)
-plt.xlabel("x-axis")
-plt.ylabel("y-axis")
-plt.title("The Title")
-#plt.savefig("filename.png",dpi=300,format="png")
-plt.show()
-'''
+
 
 
 
